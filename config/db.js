720ad318acde2ec3f483c75cb1a52fb7e1fb6f77
marchel-24 <riojuntak86@gmail.com>
2025-11-13 15:24:48 +0700

    fix:Connection DB with Neon

--- config/db.js
+++ config/db.js
@@ -1,29 +1,29 @@
-// import pkg from 'pg';
-// import dotenv from 'dotenv';
+import pkg from 'pg';
+import dotenv from 'dotenv';
 
-// dotenv.config();
-// const { Pool } = pkg;
-
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false, // wajib untuk Neon
-//   },
-// });
-
-// export default pool;
-
-
-import pg from "pg";
-import dotenv from "dotenv";
 dotenv.config();
+const { Pool } = pkg;
 
-const pool = new pg.Pool({
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PG_DATABASE,
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // wajib untuk Neon
+  },
 });
 
 export default pool;
+
+
+// import pg from "pg";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// const pool = new pg.Pool({
+//   host: process.env.PG_HOST,
+//   port: process.env.PG_PORT,
+//   user: process.env.PG_USER,
+//   password: process.env.PG_PASSWORD,
+//   database: process.env.PG_DATABASE,
+// });
+
+// export default pool;
